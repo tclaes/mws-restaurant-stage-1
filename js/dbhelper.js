182964d480e6d493c31aa7c04fed7abb08987841
Tom <tom.claes82@gmail.com>
2018-03-11 21:54:05 +0100

    fetch implemented

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -30,7 +30,7 @@ class DBHelper {
       if (error) {
         callback(error, null);
       } else {
-        const restaurant = restaurants.find(r => r.id == id);
+        const restaurant = restaurants.find(r => r.id === id);
         if (restaurant) { // Got the restaurant
           callback(null, restaurant);
         } else { // Restaurant does not exist in the database
