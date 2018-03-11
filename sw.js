b49062da7c518670d02cae89b591e6aea210b8de
Tom <tom.claes82@gmail.com>
2018-03-11 01:04:20 +0100

    sw-update

--- sw.js
+++ sw.js
@@ -1,44 +1,83 @@
-const CACHE_NAME = 'restaurant-cache';
-let urlsToCache = [
-    '/',
-    './index.html',
-    './restaurant.html',
-    './css/styles.css',
-    './js/dbhelper.js',
-    './js/main.js',
-    './js/restaurant_info.js',
-    './data/restaurants.json',
-    './imgages/1-small_small.jpg',
-    './imgages/2-small_small.jpg',
-    './imgages/3-small_small.jpg',
-    './imgages/4-small_small.jpg',
-    './imgages/5-small_small.jpg',
-    './imgages/6-small_small.jpg',
-    './imgages/7-small_small.jpg',
-    './imgages/8-small_small.jpg',
-    './imgages/9-small_small.jpg',
-    './imgages/10-small_small.jpg',
+(function() {
+    'use strict';
+
+var filesToCache = [
+    '.',
+    'index.html',
+    'restaurant.html',
+    'css/styles.css',
+    'js/dbhelper.js',
+    'js/main.js',
+    'js/restaurant_info.js',
+    'data/restaurants.json',
+    'images/1-small_small.jpg',
+    'images/2-small_small.jpg',
+    'images/3-small_small.jpg',
+    'images/4-small_small.jpg',
+    'images/5-small_small.jpg',
+    'images/6-small_small.jpg',
+    'images/7-small_small.jpg',
+    'images/8-small_small.jpg',
+    'images/9-small_small.jpg',
+    'images/10-small_small.jpg'
 ];
 
-self.addEventListener('install', function (event) {
+var staticCacheName = 'restaurant-cache';
+
+self.addEventListener('install', function(event) {
+    console.log('Attempting to install service worker and cache static assets');
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function (cache) {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
+        caches.open(staticCacheName)
+            .then(function(cache) {
+                return cache.addAll(filesToCache);
             })
     );
 });
 
-self.addEventListener('activate',  event => {
-    event.waitUntil(self.clients.claim());
-});
+    self.addEventListener('fetch', function(event) {
+        console.log('Fetch event for ', event.request.url);
+        event.respondWith(
+            caches.match(event.request).then(function(response) {
+                if (response) {
+                    console.log('Found ', event.request.url, ' in cache');
+                    return response;
+                }
+                console.log('Network request for ', event.request.url);
+                return fetch(event.request).then(function(response) {
+                    if (response.status === 404) {
+                        return caches.match('pages/404.html');
+                    }
+                    return caches.open(staticCacheName).then(function(cache) {
+                        if (event.request.url.indexOf('test') < 0) {
+                            cache.put(event.request.url, response.clone());
+                        }
+                        return response;
+                    });
+                });
+            }).catch(function(error) {
+                console.log('Error, ', error);
+                return caches.match('pages/offline.html');
+            })
+        );
+    });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request, {ignoreSearch:true}).then(response => {
-            return response || fetch(event.request);
-        })
-            .catch(err => console.log(err, event.request))
-    );
-});
+    self.addEventListener('activate', function(event) {
+        console.log('Activating new service worker...');
+
+        var cacheWhitelist = [staticCacheName];
+
+        event.waitUntil(
+            caches.keys().then(function(cacheNames) {
+                return Promise.all(
+                    cacheNames.map(function(cacheName) {
+                        if (cacheWhitelist.indexOf(cacheName) === -1) {
+                            return caches.delete(cacheName);
+                        }
+                    })
+                );
+            })
+        );
+    });
+
+
+})();
