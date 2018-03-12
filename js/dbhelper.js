d8496604ba8212f8b9bc908c65f09307f1802ca4
Tom <tom.claes82@gmail.com>
2018-03-12 22:28:16 +0100

    offline fixed

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -3,23 +3,33 @@
  */
 class DBHelper {
 
-  /**
-   * Database URL.
-   * Change this to restaurants.json file location on your server.
-   */
-  static get DATABASE_URL() {
-    const port = 1337 // Change this to your server port
-    return `http://localhost:${port}/restaurants`;
-  }
+    /**
+     * Database URL.
+     * Change this to restaurants.json file location on your server.
+     */
+    static get DATABASE_URL() {
+        const port = 3000 // Change this to your server port
+        return `http://localhost:${port}/data/restaurants.json`;
+    }
 
-  /**
-   * Fetch all restaurants.
-   */
-  static fetchRestaurants(callback) {
-      fetch(DBHelper.DATABASE_URL)
-          .then(response => response.json())
-          .then(restaurants => callback(null, restaurants));
-  }
+    /**
+     * Fetch all restaurants.
+     */
+    static fetchRestaurants(callback) {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', DBHelper.DATABASE_URL);
+        xhr.onload = () => {
+            if (xhr.status === 200) { // Got a success response from server!
+                const json = JSON.parse(xhr.responseText);
+                const restaurants = json.restaurants;
+                callback(null, restaurants);
+            } else { // Oops!. Got an error from server.
+                const error = (`Request failed. Returned status of ${xhr.status}`);
+                callback(error, null);
+            }
+        };
+        xhr.send();
+    }
 
   /**
    * Fetch a restaurant by its ID.
