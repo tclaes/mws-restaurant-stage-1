d26d8bbbf57ceb410f16f7cf7bb6eb7163a2ebd6
z411419 <tom.claes82@gmail.com>
2018-03-13 16:54:42 +0100

    gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,33 @@
-let gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
+/* file: gulpfile.js */
 
-gulp.task('default', defaultTask);
+var gulp   = require('gulp'),
+    jshint = require('gulp-jshint'),
+    sass   = require('gulp-sass');
 
-function defaultTask(done) {
-    // place code for your default task here
-    done();
-}
+// define the default task and add the watch task to it
+gulp.task('default', ['watch']);
 
-gulp.task('styles', function () {
-    gulp.src('sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+// configure the jshint task
+gulp.task('jshint', function() {
+    return gulp.src('source/javascript/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
+// configure which files to watch and what tasks to use on file changes
+gulp.task('watch', function() {
+    gulp.watch('source/javascript/**/*.js', ['jshint']);
+});
+
+gulp.task('build-css', function() {
+    return gulp.src('source/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('app/styles'));
+});
+
+/* updated watch task to include sass */
+
+gulp.task('watch', function() {
+    gulp.watch('source/js/**/*.js', ['jshint']);
+    gulp.watch('source/scss/**/*.scss', ['build-css']);
 });
